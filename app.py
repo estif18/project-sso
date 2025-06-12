3aee3ce993df224de3e12797a5be4055662806c2
estif18 <80652275+estif18@users.noreply.github.com>
2025-06-12 16:44:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,6 @@ class Submission(db.Model):
 
 class Asset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(100), nullable=False)
     code = db.Column(db.String(100), nullable=False)
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)

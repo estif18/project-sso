38a689e15931e63ac05a5f93ddc49414926fae22
estif18 <80652275+estif18@users.noreply.github.com>
2025-06-12 17:16:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,8 @@ def calcular_cumplimiento_grupo(respuestas, items):
 @app.route('/get_workers', methods=['POST'])
 def get_workers():
     company_id = request.json.get('company')
-    workers = Worker.query.filter_by(name=worker_name, company_id=company_id).first()
+    workers = Worker.query.filter_by(company_id=company_id).all()
+    worker = next((w for w in workers if w.name.strip().lower() == worker_name.strip().lower()), None)
     worker_list = [w.name for w in workers]
     return {"workers": worker_list}
 

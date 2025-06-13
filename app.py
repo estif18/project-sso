30668c7d69121f9aeea2f7545fbcc7c8037ee0a9
estif18 <80652275+estif18@users.noreply.github.com>
2025-06-13 08:01:20 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ def all_reports():
     if selected_company:
         submissions_query = submissions_query.filter_by(company_id=selected_company)
     if selected_asset:
-        submissions_query = submissions_query.filter_by(asset_id=selectedAsset)
+        submissions_query = submissions_query.filter_by(asset_id=selected_asset)
     if selected_worker:
         submissions_query = submissions_query.filter_by(worker_id=selected_worker)
     submissions = submissions_query.all()
